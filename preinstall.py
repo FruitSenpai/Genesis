9436c5c059b1382e8727510a5001f516c0f343de
FruitSenpai <941446@students.wits.ac.za>
2018-05-13 18:46:13 +0200

    Setup file for Ubuntu installation

---
+++ preinstall.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import os
+
+os.system("sudo apt-get update")
+os.system("sudo apt-get -y install python3-pip")
+os.system("sudo apt-get install python-wxgtk3.0")
+os.system("sudo apt-get install python3-numpy")
+os.system("python3 -mpip install matplotlib")
